f6379ce1a44e1a6ad87544f857abab063035078e
icylydia <icylydia@gmail.com>
2014-04-16 13:59:54 +0800

    init
    init

---
+++ ArtSearching/LCYAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  LCYAppDelegate.h
+//  ArtSearching
+//
+//  Created by eagle on 14-4-16.
+//  Copyright (c) 2014年 Duostec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ArtSearching/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ArtSearching
+//
+//  Created by eagle on 14-4-16.
+//  Copyright (c) 2014年 Duostec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LCYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LCYAppDelegate class]));
+    }
+}
