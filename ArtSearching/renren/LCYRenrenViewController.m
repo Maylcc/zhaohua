e16e21861815134d5ab5f9a9d134d4bf448439a7
icylydia <icylydia@gmail.com>
2014-04-17 13:42:27 +0800

    mm
    22

--- ArtSearching/renren/LCYRenrenViewController.m
+++ ArtSearching/renren/LCYRenrenViewController.m
@@ -10,7 +10,16 @@
 #import "LCYCommon.h"
 
 @interface LCYRenrenViewController ()
+/**
+ *  数据凶猛按钮
+ */
 @property (strong, nonatomic) IBOutlet UIView *dataFerocious;
+/**
+ *  搜索栏
+ */
+@property (weak, nonatomic) IBOutlet UISearchBar *icySearchingBar;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *searchBarVerticalSpace;
+
 @end
 
 @implementation LCYRenrenViewController
@@ -31,6 +40,15 @@
     // 添加数据凶猛按钮
     UIBarButtonItem *leftNaviButton = [[UIBarButtonItem alloc] initWithCustomView:self.dataFerocious];
     self.navigationItem.leftBarButtonItem = leftNaviButton;
+    
+    if ([[UIDevice currentDevice] orientation] == UIDeviceOrientationPortrait) {
+        [self.searchBarVerticalSpace setConstant:100];
+        NSLog(@"portrait");
+    } else {
+        [self.searchBarVerticalSpace setConstant:200];
+        NSLog(@"landscape");
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -39,9 +57,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
 #pragma mark -Actions
 - (IBAction)renrenLeftNaviButtonPressed:(id)sender{
     // TODO: 跳转到数据凶猛
 }
+
+- (IBAction)barButtonPressed:(id)sender {
+    UIBarButtonItem *item = sender;
+    NSLog(@"%ld",(long)item.tag);
+}
+
 
 @end
