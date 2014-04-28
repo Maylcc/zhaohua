29cac1432392b9b699c2bb954007ead9b83daebb
ChinaPicture <576414408@qq.com>
2014-04-28 08:49:27 +0800

    1223

---
+++ ArtSearching/InsertView.h
@@ -0,0 +1,13 @@
+//
+//  InsertView.h
+//  ArtSearching
+//
+//  Created by developer on 14-4-24.
+//  Copyright (c) 2014年 Duostec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InsertView : UIView
+
+@end
